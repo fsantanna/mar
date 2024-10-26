f4acaaa4c797594e220f51d0f48ea785986e9e1d
Francisco Sant'Anna <francisco.santanna@gmail.com>
2024-10-26 19:40:49 -0300

    - Dns: expr has type in Tuple/Union

--- src/mar/Dns.kt
+++ src/mar/Dns.kt
@@ -7,41 +7,41 @@ fun <V> Stmt.dn_collect_pos (fs: ((Stmt)->List<V>)?, fe: ((Expr)->List<V>)?, ft:
     return when (this) {
         is Stmt.Data   -> this.tp.dn_collect_pos(ft)
         is Stmt.Proto  -> this.blk.dn_collect_pos(fs,fe,ft) + this.tp.dn_collect_pos(ft)
-        is Stmt.Return -> this.e.dn_collect_pos(fe)
+        is Stmt.Return -> this.e.dn_collect_pos(fe,ft)
 
         is Stmt.Block -> this.ss.map { it.dn_collect_pos(fs,fe,ft) }.flatten()
         is Stmt.Dcl -> this.xtp?.dn_collect_pos(ft) ?: emptyList()
-        is Stmt.Set -> this.src.dn_collect_pos(fe) + this.dst.dn_collect_pos(fe)
+        is Stmt.Set -> this.src.dn_collect_pos(fe,ft) + this.dst.dn_collect_pos(fe,ft)
 
-        is Stmt.If    -> this.cnd.dn_collect_pos(fe) + this.t.dn_collect_pos(fs,fe,ft) + this.f.dn_collect_pos(fs,fe,ft)
+        is Stmt.If    -> this.cnd.dn_collect_pos(fe,ft) + this.t.dn_collect_pos(fs,fe,ft) + this.f.dn_collect_pos(fs,fe,ft)
         is Stmt.Loop  -> this.blk.dn_collect_pos(fs,fe,ft)
         is Stmt.Break -> emptyList()
 
-        is Stmt.Print -> this.e.dn_collect_pos(fe)
-        is Stmt.XExpr -> this.e.dn_collect_pos(fe)
+        is Stmt.Print -> this.e.dn_collect_pos(fe,ft)
+        is Stmt.XExpr -> this.e.dn_collect_pos(fe,ft)
         is Stmt.Nat -> emptyList()
     } + fs(this)
 }
-fun <V> Expr.dn_collect_pos (fe: ((Expr)->List<V>)?): List<V> {
+fun <V> Expr.dn_collect_pos (fe: ((Expr)->List<V>)?, ft: ((Type)->List<V>)?): List<V> {
     if (fe == null) {
         return emptyList()
     }
     return when (this) {
-        is Expr.Uno   -> this.e.dn_collect_pos(fe)
-        is Expr.Bin   -> this.e1.dn_collect_pos(fe) + this.e2.dn_collect_pos(fe)
-        is Expr.Tuple -> this.vs.map { it.dn_collect_pos(fe) }.flatten()
-        is Expr.Union -> this.v.dn_collect_pos(fe)
-        is Expr.Field -> this.col.dn_collect_pos(fe)
-        is Expr.Disc  -> this.col.dn_collect_pos(fe)
-        is Expr.Pred  -> this.col.dn_collect_pos(fe)
-        is Expr.Cons  -> this.e.dn_collect_pos(fe)
-        is Expr.Call  -> this.f.dn_collect_pos(fe) + this.args.map { it.dn_collect_pos(fe) }.flatten()
+        is Expr.Uno   -> this.e.dn_collect_pos(fe,ft)
+        is Expr.Bin   -> this.e1.dn_collect_pos(fe,ft) + this.e2.dn_collect_pos(fe,ft)
+        is Expr.Tuple -> this.vs.map { it.dn_collect_pos(fe,ft) }.flatten() + (this.xtp?.dn_collect_pos(ft) ?: emptyList())
+        is Expr.Union -> this.v.dn_collect_pos(fe,ft) + (this.xtp?.dn_collect_pos(ft) ?: emptyList())
+        is Expr.Field -> this.col.dn_collect_pos(fe,ft)
+        is Expr.Disc  -> this.col.dn_collect_pos(fe,ft)
+        is Expr.Pred  -> this.col.dn_collect_pos(fe,ft)
+        is Expr.Cons  -> this.e.dn_collect_pos(fe,ft)
+        is Expr.Call  -> this.f.dn_collect_pos(fe,ft) + this.args.map { it.dn_collect_pos(fe,ft) }.flatten()
         is Expr.Acc, is Expr.Nat, is Expr.Null, is Expr.Unit,
         is Expr.Bool, is Expr.Char, is Expr.Num -> emptyList()
-        is Expr.Create -> this.co.dn_collect_pos(fe)
-        is Expr.Start  -> this.args.map { it.dn_collect_pos(fe) }.flatten() + this.exe.dn_collect_pos(fe)
-        is Expr.Resume -> this.arg.dn_collect_pos(fe) + this.exe.dn_collect_pos(fe)
-        is Expr.Yield  -> this.arg.dn_collect_pos(fe)
+        is Expr.Create -> this.co.dn_collect_pos(fe,ft)
+        is Expr.Start  -> this.args.map { it.dn_collect_pos(fe,ft) }.flatten() + this.exe.dn_collect_pos(fe,ft)
+        is Expr.Resume -> this.arg.dn_collect_pos(fe,ft) + this.exe.dn_collect_pos(fe,ft)
+        is Expr.Yield  -> this.arg.dn_collect_pos(fe,ft)
 
     } + fe(this)
 }
@@ -70,9 +70,10 @@ fun Stmt.dn_visit_pos (fs: ((Stmt)->Unit)?, fe: ((Expr)->Unit)?, ft: ((Type)->Un
         if (ft == null) null else ({ ft(it) ; emptyList() })
     )
 }
-fun Expr.dn_visit_pos (fe: ((Expr)->Unit)?) {
+fun Expr.dn_visit_pos (fe: ((Expr)->Unit)?, ft: ((Type)->Unit)?) {
     this.dn_collect_pos (
-        if (fe == null) null else ({ fe(it) ; emptyList<Unit>() })
+        if (fe == null) null else ({ fe(it) ; emptyList<Unit>() }),
+        if (ft == null) null else ({ ft(it) ; emptyList<Unit>() }),
     )
 }
 
